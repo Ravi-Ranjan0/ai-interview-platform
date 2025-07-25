ca77fe21c7f5127620a6efcbc0fbd5dde28b83bb
Ravi Ranjan <raviranjanxiia20@gmail.com>
2025-07-25 20:39:49 +0530

    12: Agent Update

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -56,6 +56,23 @@ export const AgentForm = ({
     })
   );
 
+
+  const updateAgent = useMutation(
+    trpc.agents.update.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+            trpc.agents.getMany.queryOptions({})
+        );
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(error.message);
+
+        //TODO :Check if error code is "FORBIDDEN," redirect to "/upgrade"
+      },
+    })
+  );
+
   const form = useForm<z.infer<typeof agentsInsertSchema>>({
     resolver: zodResolver(agentsInsertSchema),
     defaultValues: {
@@ -65,11 +82,12 @@ export const AgentForm = ({
   });
 
   const isEdit = !!initialValues?.id;
-  const isPending = createAgent.isPending;
+  const isPending = createAgent.isPending || updateAgent.isPending;
 
   const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
     if (isEdit) {
       // Handle update logic here
+      updateAgent.mutate({ ...values, id: initialValues.id });
       console.log("Update agent:", values);
     } else {
       createAgent.mutate(values);
